c78f556cf3b324f832500cc0d72658af38286e1e
Dmitry Liskovich <Dima_liskovich@mail.ru>
2019-05-26 12:15:14 +0300

    Login adding

---
+++ src/login/Login.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import render from'react-dom';
+import './Login.css';
+
+
+function Login (){
+    return(
+        <div className='login' id='login'>
+            <div className='login-form'>
+                <h2>Sign in with</h2>
+                <div className='social-list'>
+                    <ul>
+                        <li><i className='fab fa-vk'></i></li>
+                        <li><i className='fab fa-facebook-f'></i></li>
+                        <li><i className='fab fa-google'></i></li>
+                    </ul>
+                </div>
+                <h2>OR</h2>
+                <div className='under-line'></div>
+                <form action='/login' className = 'form'>
+                    <label>User name or email</label>
+                    <input type='email' placeholder='Name' name='userName' className='input'></input>
+                    <div className='input-line-name'></div>
+                    <label>Password</label>
+                    <input type='password' placeholder='Password' name='password' className='input'></input>
+                    <div className='input-line-password'></div>
+                    <input type='submit' className ='submit-btn' value='Submit'></input>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+
+
+export default Login;

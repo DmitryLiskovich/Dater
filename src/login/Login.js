b3bca3e394eadd1aa6b176057dda4438e692a16d
Dmitry Liskovich <Dima_liskovich@mail.ru>
2019-05-27 21:45:21 +0300

    Create sign up page

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,33 +1,39 @@
 import React from 'react';
 import render from'react-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './scss/login.scss';
+import Signup from '../signup/Signup';
 
 
 function Login (){
     return(
-        <div className='login' id='login'>
-            <div className='login-form'>
-                <h2>Sign in with</h2>
-                <div className='login-social-list'>
-                    <ul>
-                        <li><i className='fab fa-vk'></i></li>
-                        <li><i className='fab fa-facebook-f'></i></li>
-                        <li><i className='fab fa-google'></i></li>
-                    </ul>
+            <div className='login' id='login'>
+                <div className='login-form'>
+                    <h2>Log in with</h2>
+                    <div className='login-social-list'>
+                        <ul>
+                            <li><i className='fab fa-vk'></i></li>
+                            <li><i className='fab fa-facebook-f'></i></li>
+                            <li><i className='fab fa-google'></i></li>
+                        </ul>
+                    </div>
+                    <h3>OR</h3>
+                    <div className='login-under-line'></div>
+                    <form method='POST' action='/login' className = 'login-sign-in-form'>
+                        <i className='fas fa-user login-input-icon'></i>
+                        <input type='text' placeholder='Name' name='userName' className='login-input'></input>
+                        <div className='login-input-underline'></div>
+                        <i className='fas fa-lock login-input-icon'></i>
+                        <input type='password' placeholder='Password' name='password' className='login-input'></input>
+                        <div className='login-input-underline'></div>
+                        <input type='submit' className ='login-submit-btn' value='Submit'></input>
+                    </form>
+                    <p>Have not account?</p>
+                    <Link to='/sign-up'>
+                        <p className='login-form-sign-up'>SIGN UP</p>
+                    </Link>
                 </div>
-                <h2>OR</h2>
-                <div className='login-under-line'></div>
-                <form method='POST' action='/login' className = 'login-sign-in-form'>
-                    <label>User name or email</label>
-                    <input type='email' placeholder='Name' name='userName' className='login-input'></input>
-                    <div className='login-input-underline'></div>
-                    <label>Password</label>
-                    <input type='password' placeholder='Password' name='password' className='login-input'></input>
-                    <div className='login-input-underline'></div>
-                    <input type='submit' className ='login-submit-btn' value='Submit'></input>
-                </form>
             </div>
-        </div>
     );
 };
 
